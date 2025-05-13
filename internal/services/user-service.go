cced883abb89a970c0da2a667fb8cafc6de28e1f
Patrick Phan <phankieuphu@gmail.com>
2025-05-14 01:52:00 +0700

    [ADD] Finish register user implement with gateway

--- internal/services/user-service.go
+++ internal/services/user-service.go
@@ -2,64 +2,104 @@ package services
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
-	"net/http"
+	"strings"
 	"sync"
 	"time"
 
 	config "github.com/phankieuphu/ecom-user/configs"
-	userPb "github.com/phankieuphu/ecom-user/gen/user"
+	userPb "github.com/phankieuphu/ecom-user/gen/user/v1"
+	"github.com/phankieuphu/ecom-user/internal/logger"
 	"github.com/phankieuphu/ecom-user/internal/models"
-
-	"github.com/gin-gonic/gin"
 )
 
-type RegisterRequest struct {
-	Email string `json:"email" binding:"required,email"`
+func sendEmail(ctx context.Context, u *models.User) error {
+	logger := logger.NewConsoleLogger()
+
+	for {
+		select {
+		case <-time.After(5 * time.Second):
+			// time.Sleep(5 * time.Second)
+			if u.Email != "" {
+				logger.Info("email service", "user", u)
+			}
+			logger.Info("Send email to user success", "user", u)
+			return nil
+		case <-ctx.Done():
+			return fmt.Errorf("email sending canceled: %v", ctx.Err())
+		}
+	}
+}
+func writeToLogService(u *models.User) error {
+	logger := logger.NewConsoleLogger()
+	logger.Info("log service", "user", u)
+	return nil
 }
 
-func RegisterUser(c *gin.Context) {
-	var req models.UserRegisterValidate
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+func cacheUser(ctx context.Context, u *models.User) error {
+	fmt.Println("set cache to service")
+	for {
+		select {
+		case <-time.After(2 * time.Second):
+			log.Printf("[cache] User cached: %d", u.ID)
+			return nil
+		case <-ctx.Done():
+			return fmt.Errorf("cache canceled: %v", ctx.Err())
+
+		}
 	}
+}
+
+type UserService struct {
+	userPb.UnimplementedUserServer
+}
+
+func (s *UserService) GetProfileUser(ctx context.Context, req *userPb.GetProfileUserRequest) (*userPb.GetProfileUserResponse, error) {
+	fmt.Println("req", req)
+	return &userPb.GetProfileUserResponse{
+		Message: "Hello " + req.GetName(),
+	}, nil
+}
+func (s *UserService) RegisterUser(ctx context.Context, req *userPb.RegisterUserRequest) (*userPb.RegisterUserResponse, error) {
+	//  req models.UserRegisterValidate
+	// if err := c.ShouldBindJSON(&req); err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	// 	return
+	// }
+	reqUser := req.User
+	fmt.Println("user", reqUser)
 	errChan := make(chan error, 5)
 	var wg sync.WaitGroup
 	// if err := c.ShouldBindJSON(&models.User); err != nil {
 	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	// }
-
+	// hash user password with bcrypt
 	user := &models.User{
-		Email:       req.Email,
-		Password:    req.Password,
-		Username:    req.Username,
-		Address:     req.Address,
-		PhoneNumber: req.PhoneNumber,
-		DisplayName: req.DisplayName,
+		Email:       reqUser.Email,
+		Password:    reqUser.Password,
+		Username:    reqUser.Username,
+		Address:     reqUser.Address,
+		PhoneNumber: reqUser.PhoneNumber,
+		DisplayName: "",
 	}
 
-	ctx, cancel := context.WithTimeout(c.Request.Context(), 6*time.Second)
-	defer cancel()
+	//ctx, cancel := context.WithTimeout(c.Request.Context(), 6*time.Second)
+	// defer cancel()
 	database := config.GetDb()
 
 	userSaved := database.Save(user)
 	fmt.Println("userSaved", userSaved)
 	if userSaved.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": userSaved.Error.Error(),
-		})
-		return
+		return nil, userSaved.Error
 	}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		if err := sendEmail(ctx, user); err != nil {
-			errChan <- fmt.Errorf("log error: %w", err)
-		}
-	}()
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	if err := sendEmail(ctx, user); err != nil {
+	// 		errChan <- fmt.Errorf("log error: %w", err)
+	// 	}
+	// }()
 
 	wg.Add(1)
 	go func() {
@@ -89,54 +129,9 @@ func RegisterUser(c *gin.Context) {
 	}
 
 	if len(errors) > 0 {
-		c.JSON(http.StatusInternalServerError, gin.H{"status": "partial failure", "errors": errors})
-		return
+		return nil, fmt.Errorf("encountered errors: %s", strings.Join(errors, "; "))
+
 	}
 
-	c.JSON(http.StatusOK, gin.H{"status": "registered", "user": user.ID})
-}
-
-func sendEmail(ctx context.Context, u *models.User) error {
-	for {
-		select {
-		case <-time.After(5 * time.Second):
-			// time.Sleep(5 * time.Second)
-			if u.Email != "" {
-				fmt.Println("Not correct user")
-				return errors.New("not correct user")
-			}
-			return nil
-		case <-ctx.Done():
-			return fmt.Errorf("email sending canceled: %v", ctx.Err())
-		}
-	}
-}
-func writeToLogService(u *models.User) error {
-	fmt.Println("write log to service")
-	return nil
-}
-
-func cacheUser(ctx context.Context, u *models.User) error {
-	fmt.Println("set cache to service")
-	for {
-		select {
-		case <-time.After(2 * time.Second):
-			log.Printf("[cache] User cached: %d", u.ID)
-			return nil
-		case <-ctx.Done():
-			return fmt.Errorf("cache canceled: %v", ctx.Err())
-
-		}
-	}
-}
-
-type UserService struct {
-	userPb.UnimplementedUserServer
-}
-
-func (s *UserService) GetProfileUser(ctx context.Context, req *userPb.GetProfileUserRequest) (*userPb.GetProfileUserResponse, error) {
-	fmt.Println("req", req)
-	return &userPb.GetProfileUserResponse{
-		Message: "Hello " + req.GetName(),
-	}, nil
+	return &userPb.RegisterUserResponse{ID: user.ID.String(), Message: "Success"}, nil
 }
